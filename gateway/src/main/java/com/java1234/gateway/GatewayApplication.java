078a8efe7c125c413bdd1c15377a2a8c5bfdc09a
xrl6238 <3jR0$am8>
2018-09-12 09:53:26 +0800

    完成路由过滤器

--- gateway/src/main/java/com/java1234/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/java1234/gateway/GatewayApplication.java
@@ -1,9 +1,11 @@
 package com.java1234.gateway;
 
+import com.java1234.gateway.filter.AccessFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -12,5 +14,10 @@ public class GatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+	@Bean
+	public AccessFilter accessFilter() {
+		return new AccessFilter();
 	}
 }
