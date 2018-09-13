cd306a9e6132693da549df68afac0ad264cbc7c0
xrl6238 <3jR0$am8>
2018-09-13 10:09:49 +0800

    config

--- provider/src/main/java/com/java1234/controller/StudentProviderController.java
+++ provider/src/main/java/com/java1234/controller/StudentProviderController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import com.java1234.common.entity.FaceLibrary;
 import com.java1234.service.FaceLibraryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,10 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class StudentProviderController {
     @Autowired
     private FaceLibraryService faceLibraryService;
+    @Value("${server.port}")
+    private String foo;
 
     @GetMapping(value="/list")
     public List<FaceLibrary> list(){
-        System.out.println("----------provider---------");
+        System.out.println("----------provider---------"+foo);
         return faceLibraryService.queryForList("");
     }
      
